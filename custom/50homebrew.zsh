e616c27b1cb1febf38ac17abfca2b6f72bd62c34
Graham Hughes <graham@rightscale.com>
2010-09-08 09:18:20 -0700

    Make SBCL_HOME refer to brew_home.

--- custom/50homebrew.zsh
+++ custom/50homebrew.zsh
@@ -1,4 +1,4 @@
 brew_home=/opt/local
 export PATH="$brew_home/bin:$PATH"
-export SBCL_HOME=/opt/local/lib/sbcl
+export SBCL_HOME=$brew_home/lib/sbcl
 export MANPATH="$brew_home/man:$brew_home/share/man:$MANPATH"
